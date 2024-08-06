dab09b86b544963811fc5b18d70d8805e696b72f
Vladimir <vovandro@gmail.com>
2024-08-06 20:26:53 +0300

    add colored log

--- logger/std.go
+++ logger/std.go
@@ -6,9 +6,20 @@ import (
 	"time"
 )
 
+var levelColor map[interfaces.LogLevel]string = map[interfaces.LogLevel]string{
+	interfaces.DEBUG:    "\033[35m",
+	interfaces.INFO:     "\033[36m",
+	interfaces.WARNING:  "\033[33m",
+	interfaces.MESSAGE:  "\033[32m",
+	interfaces.ERROR:    "\033[31m",
+	interfaces.CRITICAL: "\033[31m",
+}
+
 type Std struct {
 	Parent interfaces.ILogger
 	Config StdConfig
+	queue  chan string
+	done   chan interface{}
 }
 
 type StdConfig struct {
@@ -23,6 +34,11 @@ func NewStd(cfg StdConfig, parent interfaces.ILogger) *Std {
 }
 
 func (t *Std) Init() error {
+	t.queue = make(chan string, 1000)
+	t.done = make(chan interface{})
+
+	t.run()
+
 	if t.Parent != nil {
 		return t.Parent.Init()
 	}
@@ -35,7 +51,26 @@ func (t *Std) Stop() error {
 		return t.Parent.Stop()
 	}
 
+	close(t.done)
+	time.Sleep(time.Second * 1)
+	close(t.queue)
+
 	return nil
+}
+
+func (t *Std) run() {
+	go func() {
+		for {
+			select {
+			case m := <-t.queue:
+				fmt.Println(m)
+
+			case <-t.done:
+				time.Sleep(time.Millisecond * 100)
+				return
+			}
+		}
+	}()
 }
 
 func (t *Std) SetLevel(level interfaces.LogLevel) {
@@ -47,7 +82,7 @@ func (t *Std) SetLevel(level interfaces.LogLevel) {
 }
 
 func (t *Std) Push(level interfaces.LogLevel, msg string, toParent bool) {
-	fmt.Printf("%s [%s] %s\n", level.String(), time.Now().Format("2006-01-02 15:04.05"), msg)
+	fmt.Printf("%s%s\u001B[0m [\033[37m%s\033[0m] %s\n", levelColor[level], level.String(), time.Now().Format("2006-01-02 15:04.05"), msg)
 
 	if toParent && t.Parent != nil {
 		t.Parent.Push(level, msg, true)
