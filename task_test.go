d2db361664440e80bc172cf4e1a65ccdba50413e
Vladimir <vovandro@gmail.com>
2024-08-25 00:27:42 +0300

    fix test

--- task_test.go
+++ task_test.go
@@ -2,12 +2,13 @@ package Paranoia
 
 import (
 	"gitlab.com/devpro_studio/Paranoia/interfaces"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 type testTask struct {
-	count int
+	count atomic.Int32
 	cfg   []interfaces.ITaskRunConfiguration
 }
 
@@ -15,7 +16,7 @@ func (t *testTask) Init(app interfaces.IService) error        { return nil }
 func (t *testTask) Stop() error                               { return nil }
 func (t *testTask) String() string                            { return "test" }
 func (t *testTask) Start() []interfaces.ITaskRunConfiguration { return t.cfg }
-func (t *testTask) Invoke(map[string]interface{})             { t.count++ }
+func (t *testTask) Invoke(map[string]interface{})             { t.count.Add(1) }
 
 func Test_task_run(t1 *testing.T) {
 	reset := make(chan time.Duration, 1)
@@ -43,15 +44,15 @@ func Test_task_run(t1 *testing.T) {
 		for i := 0; i < 5; i++ {
 			time.Sleep(time.Second)
 
-			if tsk.count == 2 {
+			if tsk.count.Load() == 2 {
 				break
 			}
 		}
 
 		t.Stop()
 
-		if tsk.count != 2 {
-			t1.Errorf("expect 2 tasks, got %d", tsk.count)
+		if tsk.count.Load() != 2 {
+			t1.Errorf("expect 2 tasks, got %d", tsk.count.Load())
 		}
 	})
 }
@@ -72,8 +73,8 @@ func Test_task_ManualRun(t1 *testing.T) {
 		_ = t.RunTask("test", nil)
 		time.Sleep(time.Second * 2)
 
-		if tsk.count != 1 {
-			t1.Errorf("expect 1 tasks, got %d", tsk.count)
+		if tsk.count.Load() != 1 {
+			t1.Errorf("expect 1 tasks, got %d", tsk.count.Load())
 		}
 
 		t.Stop()
