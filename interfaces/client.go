ddcd07ca18335bd84fdf9a3bec3fd73d7c79b7ad
Vladimir <vovandro@gmail.com>
2024-09-05 00:24:41 +0300

    add grpc client and test

--- interfaces/client.go
+++ interfaces/client.go
@@ -1,13 +1,24 @@
 package interfaces
 
-import "context"
+import (
+	"context"
+	"google.golang.org/grpc"
+)
 
 type IClient interface {
 	Init(app IEngine) error
 	Stop() error
 	String() string
+}
 
+type IClientBase interface {
+	IClient
 	Fetch(ctx context.Context, method string, host string, data []byte, headers map[string][]string) chan IClientResponse
+}
+
+type IClientGrpc interface {
+	IClient
+	GetClient() *grpc.ClientConn
 }
 
 type IClientResponse interface {
